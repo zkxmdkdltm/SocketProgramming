6ecdb27abf3311fbe3aa2b89fe60963a3b274cc7
zlxmdkdltm <zkxmdkdltm@gmail.com>
2022-12-29 00:01:11 +0900

    feat: Server response를 Client로 전송

--- src/Client.java
+++ src/Client.java
@@ -4,17 +4,22 @@ import java.net.Socket;
 import java.util.Scanner;
 
 /**
- * 1 대 1 소켓 통신 서버 예제
+ * 1 대 1 소켓 통신 클라이언트 예제
  */
 public class Client {
     public static void main(String[] args) {
+        // 소켓 및 input/output 스트림 선언 (클라이언트 소켓은 서버소켓이 필요없다)
         Socket socket = null;
+        BufferedReader in = null;
         BufferedWriter out = null;
         Scanner sc = new Scanner(System.in);
 
         try{
+            // Port 번호로 소켓 생성
             socket = new Socket("localhost", 5555);
 
+            // input/output 스트림 생성
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
             while(true){
@@ -23,7 +28,7 @@ public class Client {
                 String outMsg = sc.nextLine();
 
                 // 종료 메세지
-                if(outMsg.equals("BYE")){
+                if(outMsg.equalsIgnoreCase("bye")){
                     out.write(outMsg + "\n");
                     out.flush();
                     System.out.println("종료");
@@ -34,6 +39,10 @@ public class Client {
                 out.write(outMsg + "\n");
                 out.flush();
 
+                // Server의 Response 메시지 수신 및 출력
+                String inMsg = in.readLine();
+                System.out.println("서버 >> : " + inMsg);
+
 
             }
 
@@ -41,7 +50,9 @@ public class Client {
             System.out.println(e.getMessage());
         } finally {
             try{
+                // 소켓및 input/output stream 종료
                 sc.close();
+                in.close();
                 out.close();
                 socket.close();
             } catch(IOException e) {
--- src/Server.java
+++ src/Server.java
@@ -13,7 +13,7 @@ public class Server {
         ServerSocket server = null;
         Socket socket = null;
 
-        // reader, writer 선언
+        // 데이터 송수신을 위한 input/output 스트림 선언
         BufferedReader in = null;
         BufferedWriter out = null;
         Scanner sc = new Scanner(System.in);
@@ -27,42 +27,49 @@ public class Server {
             socket = server.accept();
             System.out.println("연결 됨.");
 
-            // 입력, 출력 준비
+            // input/output 스트림 생성
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
             while(true){
                 // Client의 Request 수신
-                String msg = in.readLine();
+                String inMsg = in.readLine();
 
                 // GET/HEAD/POST/PUT 의 Request 에 대한 Response
-                if(msg.equals("GET-응답 200")){
-                    System.out.println("HTTP/1.1 200 OK");
-                } else if(msg.equals("GET-응답 404")){
-                    System.out.println("HTTP/1.1 404 Not Found");
-                } else if(msg.equals("HEAD-응답 100")){
-                    System.out.println("HTTP/1.1 100 Continue");
-                } else if(msg.equals("HEAD-응답 101")){
-                    System.out.println("HTTP/1.1 101 Switching Protocols");
-                } else if(msg.equals("POST-응답 500")){
-                    System.out.println("HTTP/1.1 500 Internal Server Error");
-                } else if(msg.equals("POST-응답 400")){
-                    System.out.println("HTTP/1.1 400 Bad Request");
-                } else if(msg.equals("PUT-응답 301")){
-                    System.out.println("HTTP/1.1 301 Moved Permanently");
-                } else if(msg.equals("PUT-응답 503")){
-                    System.out.println("HTTP/1.1 503 Service Unavailable");
-                } else if(msg.equals("BYE")){
+                if(inMsg.equalsIgnoreCase("bye")) {
                     // 종료 메세지
                     System.out.println("종료");
+                    out.write("종료" + "\n");
+                    out.flush();
                     break;
                 }
+                System.out.println("클라이언트 >> : " + inMsg);
+
+                if(inMsg.equals("GET-응답 200")){
+                    out.write("HTTP/1.1 200 OK" + "\n");
+                } else if(inMsg.equals("GET-응답 404")){
+                    out.write("HTTP/1.1 404 Not Found" + "\n");
+                } else if(inMsg.equals("HEAD-응답 100")){
+                    out.write("HTTP/1.1 100 Continue" + "\n");
+                } else if(inMsg.equals("HEAD-응답 101")){
+                    out.write("HTTP/1.1 101 Switching Protocols" + "\n");
+                } else if(inMsg.equals("POST-응답 500")){
+                    out.write("HTTP/1.1 500 Internal Server Error" + "\n");
+                } else if(inMsg.equals("POST-응답 400")){
+                    out.write("HTTP/1.1 400 Bad Request" + "\n");
+                } else if(inMsg.equals("PUT-응답 301")){
+                    out.write("HTTP/1.1 301 Moved Permanently" + "\n");
+                } else if(inMsg.equals("PUT-응답 503")){
+                    out.write("HTTP/1.1 503 Service Unavailable" + "\n");
+                }
+                out.flush();
             }
 
         } catch(IOException e) {
             System.out.println(e.getMessage());
         } finally {
             try{
+                // 소켓, 서버 및 input/output stream 종료
                 sc.close();
                 in.close();
                 out.close();
